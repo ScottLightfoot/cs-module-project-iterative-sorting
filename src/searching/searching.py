63b825c72bcd43b4611034cb27dd1d41af6be4b5
ScottLightfoot <gslightfoot@gmail.com>
2020-08-18 14:59:00 -0500

    fixed for actual complete assignment with stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    lower_bound = 0
+    upper_bound = len(arr)
+    mid = int(upper_bound/2)
 
-    # Your code here
+    while upper_bound - lower_bound > 0:
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            upper_bound = mid
+            mid = int(upper_bound/2)
+        else:
+            lower_bound = mid
+            mid += int((upper_bound - lower_bound)/2)
 
-
-    return -1  # not found
+    else:
+        return -1
